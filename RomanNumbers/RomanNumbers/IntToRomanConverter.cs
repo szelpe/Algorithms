9db524fb4f40b7fc5497267305377539b204616e
Peter Szel <peter.szel@greenfoxacademy.com>
2020-07-19 16:58:28 +0200

    Added IntToRoman

---
+++ RomanNumbers/RomanNumbers/IntToRomanConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumbers
+{
+    class IntToRomanConverter
+    {
+        private static List<(string, int)> digits = new List<(string, int)>() {
+                 ( "M", 1000 ),
+                 ( "CM", 900 ),
+                 ( "D", 500 ),
+                 ( "CD", 400 ),
+                 ( "C", 100 ),
+                 ( "XC", 90 ),
+                 ( "L", 50 ),
+                 ( "XL", 40 ),
+                 ( "X", 10 ),
+                 ( "IX", 9 ),
+                 ( "V", 5 ),
+                 ( "IV", 4 ),
+                 ( "III", 3 ),
+                 ( "II", 2 ),
+                 ( "I", 1 ),
+        };
+
+        public string IntToRoman(int num)
+        {
+            var builder = new StringBuilder();
+            int remaining = num;
+            foreach (var (digit, value) in digits)
+            {
+                for (int i = 0; remaining >= value; i++)
+                {
+                    builder.Append(digit);
+                    remaining -= value;
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ RomanNumbers/RomanNumbers/IntToRomanTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RomanNumbers
+{
+    public class IntToRomanTests
+    {
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(5, "V")]
+        [InlineData(4, "IV")]
+        [InlineData(58, "LVIII")]
+        [InlineData(9, "IX")]
+        [InlineData(1994, "MCMXCIV")]
+        public void Test(int num, string expected)
+        {
+            var converter = new IntToRomanConverter();
+
+            string result = converter.IntToRoman(num);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
